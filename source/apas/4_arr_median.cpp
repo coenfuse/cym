446dca6fe3013ad9ddcb9df540886957ecb39463
coenfuse <65540834+coenfuse@users.noreply.github.com>
2022-06-04 20:29:33 +0530

    array_median

---
+++ source/apas/4_arr_median.cpp
@@ -0,0 +1,61 @@
+// There are two sorted array num1 and num2 of size m and n respectively.
+// Find the median of the two sorted arrays. The overall run time complexity
+// should be O(log(m+n))
+// You may assume num1 and num2 cannot be both empty.
+//
+// eg;
+// num1 = [1,3]
+// num2 = [2]
+// median = 2.0
+//
+// num1 = [1,2]
+// num2 = [3,4]
+// median = (2 + 3)/2 = 2.5
+
+#include <iostream>
+#include <set>
+#include <vector>
+
+using arr_int = std::vector<int>;
+
+double arr_median(arr_int arr_a, arr_int arr_b)
+{
+    double median = 0;
+    arr_int data;
+    std::set<int> tmp;
+
+    // Create a unique and sorted data var that contains both array elements 
+    for(auto digit : arr_a)
+        tmp.insert(digit);
+
+    for(auto digit : arr_b)
+        tmp.insert(digit);
+
+    // Create an array from std::set since set isn't index accessible
+    // Could've been possibly avoided if used hash-map
+    for(auto digit : tmp)
+        data.push_back(digit);
+
+    // Find middle point of data. 
+    // If the size of data is an even number, take the average of two middlemost
+    // elements, else return the middle item
+    if(data.size() % 2 == 0)                             // checking even or odd
+    {
+        auto mid_a = data[int(data.size() / 2) - 1];
+        auto mid_b = data[int(data.size() / 2)];
+
+        median = (mid_a + mid_b) / 2.0;
+    }
+    else
+        median = data[int(data.size() / 2)];            // find middle index, convert index to num then access data at that index
+
+    return median;
+}
+
+
+int main(void)
+{
+    std::cout << arr_median({1,3}, {2,4}) << std::endl;
+
+    return 0;
+}

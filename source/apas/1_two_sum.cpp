e63f0ac84d5f72e30488906745ed37245e20a3fc
coenfuse <65540834+coenfuse@users.noreply.github.com>
2022-07-07 21:10:49 +0530

    improve two_sum

--- source/apas/1_two_sum.cpp
+++ source/apas/1_two_sum.cpp
@@ -9,38 +9,67 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
 #include <set>
+
 
 std::vector<int> two_sum(const std::vector<int> arr, const int target)
 {
     std::vector<int> indices;
-    std::set<int> data_index;
+    std::map<int, std::set<int>> table;
 
-    // Create a set in O(n) time to search for complements in O(1) time.
-    for(auto item : arr)
-        data_index.insert(item);
-
-    // Find pairs by creating complements and searching them in index, if found
-    // run a linear scan to find the index of the complement. Terminate the loop
-    // there then.
-    for(int i = 0; i < arr.size(); i++)                                         
+    // Fill the map with values and index pair, this is required during searches
+    for(int i = 0; i < arr.size(); i++)
     {
-        auto complement = target - arr[i];
-        
-        if (data_index.find(complement) != data_index.end())                    
-        {
-            int j = i;
-            
-            for(; j < arr.size(); j++)                                          
-                if (arr[j] == complement)
-                    break;
+        // Check if value is already present in the map. If it is present then
+        // it means we've found duplicate value. So we just add the new index to
+        // its indexes list.
+        // If the object is not found, then we create a new {value:index_list}
+        // pair. So, if the initial array was like this, [1,4,3,1,4,2,3,4]
+        // the map would look like:
+        // 1 : {0}
+        // 2 : {5}
+        // 3 : {2,6}
+        // 4 : {1,4,7}
+        if(table.find(arr[i]) != table.end())
+            table[arr[i]].insert(i);
+        else
+            table.insert({arr[i], {i}});
+    }
 
-            indices = {i, j};
+    // Linear scan for complement pairs
+    for(int i = 0; i < arr.size(); i++)
+    {
+        auto complement = target - arr[i];                                     
+
+        // Find complement in table
+        if(table.find(complement) != table.end())                               
+        {
+            auto index_list = table[complement];                               
+            
+            // If more than one complements are present, check if the current index
+            // is in the list or not. If it is, remove it first (avoid duplicate
+            // indexes) and then take the first index from the list
+            if(index_list.size() > 1)
+            {
+                if(index_list.find(i) != index_list.end())
+                    index_list.erase(index_list.find(i));                       
+
+                indices = {i, *(index_list.begin())};                           
+            }
+            // else set the first value from index list as the complement index
+            // given it is not as same as the current index (avoid duplicate index)
+            else
+            {
+                if(*(index_list.begin()) != i)
+                    indices = {i, *(index_list.begin())};                           
+            }
         }
         else
             continue;
 
-        if (!indices.empty())
+        // pairs have been found, exit
+        if(!indices.empty())
             break;
     }
 
@@ -51,7 +80,7 @@ std::vector<int> two_sum(const std::vector<int> arr, const int target)
 // Driver code
 int main(void)
 {
-    auto output = two_sum({-9,5,0,18}, 9);
+    auto output = two_sum({3,2,4}, 6);
 
     std::cout << output[0] << std::endl;
     std::cout << output[1] << std::endl;
